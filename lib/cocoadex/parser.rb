c17c053f983086086080c2cd65470ff9f7f8c7c7
Delisa Mason <iskanamagus@gmail.com>
2012-07-19 18:14:04 -0400

    Simplify parser logic blocks

--- lib/cocoadex/parser.rb
+++ lib/cocoadex/parser.rb
@@ -21,32 +21,39 @@ module Cocoadex
       (File.basename(path) == 'index.html' && File.exist?(File.join(File.dirname(path),'Reference')))
     end
 
+    def self.class_ref? title
+      title.include? "Class Reference" or title.include? "Protocol Reference"
+    end
+
     def self.parse docset_path
-      logger.debug "Parsing docset..."
+      logger.info "Parsing docset tokens (This may take a moment)..."
       plist = File.join(docset_path,"Contents", "Info.plist")
       if File.exist? plist
         docset = DocSet.new(plist)
-        logger.info docset.inspect
+
         files = Dir.glob(docset_path+"/**/*.html")
         files.reject! {|f| ignored?(f) }
-
-        files.each_with_index do |f,index|
-          logger.debug "  Parsing path: #{f}"
-          doc = Nokogiri::HTML(IO.read(f))
-          title_nodes = doc.css("#IndexTitle")
-          unless title_nodes.size == 0
-            title = title_nodes.first['content']
-            if title.include? "Deprecated"
-              next
-            elsif title.include? "Class Reference" or title.include? "Protocol Reference"
-              logger.info "    Creating #{title}"
-              Keyword.tokenize_class docset.name, f, index
-            end
-          end
+        files.each_with_index do |f,i|
+          index_html(docset,f,i)
         end
 
         logger.info "Tokens Indexed: #{Keyword.datastore.size}"
         Keyword.write
+      end
+    end
+
+    def self.index_html docset, path, index
+      logger.debug "  Parsing path: #{path}"
+
+      doc = Nokogiri::HTML(IO.read(path))
+      title_nodes = doc.css("#IndexTitle")
+      unless title_nodes.size == 0
+        title = title_nodes.first['content']
+
+        if class_ref? title
+          # logger.info "Caching #{title}"
+          Keyword.tokenize_class docset.name, path, index
+        end
       end
     end
   end
